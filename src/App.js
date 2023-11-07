340ae33855be0ecdc4ec3c2a671615f934d72779
Kaustubhya Shukla <kaustubhyaksd@gmail.com>
2023-11-07 11:57:54 +0530

    here

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
 
+let name = "Harry";
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React with KSD
-        </a>
-      </header>
+    <>
+    <nav>
+      <li>Home</li>
+      <li>About Us</li>
+      <li>Contact</li>
+    </nav>
+    <div className="container">
+      <h1>Hello {name}</h1>
+      <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Asperiores, aperiam quidem. Provident voluptatum quas molestiae blanditiis nihil. Voluptates beatae reprehenderit veniam necessitatibus quam pariatur consequuntur, temporibus perspiciatis sed iure quo. </p>
     </div>
+    </>
   );
 }
 
